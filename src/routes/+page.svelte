60f53c1278d859e6998c0db335296e2bdc337029
Alberto Barrago <albertobarrago@gmail.com>
2024-12-30 19:12:31 +0100

    Changed version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
         "Cloud Solutions",
         "Team Leadership"
     ];
-    let version = "v1.0.0"
+    let version = "v1.1.0"
     let email = "albertobarrago@gmail.com";
 
 
@@ -34,7 +34,6 @@
         script.async = true;
         document.body.appendChild(script);
     });
-
 </script>
 
 <main class="container">
@@ -61,8 +60,8 @@
                 <a href="{githubRepoString}" target="_blank" class="btn primary">View
                     Projects
                 </a>
-                <a href="mailto:albertobarrago@gmail.com?subject=Let%27s%20Collaborate&body=Hi%20Alberto%2C%0A%0AI%20would%20like%20to%20discuss%20a%20project%20with%20you.%0A%0ABest%20regards%2C%0AYour%20Name" class="btn secondary">
-                    Get in Touch
+                <a href="mailto:albertobarrago@gmail.com?subject=Let's%20Collaborate&body=Hi%20Alberto%2C%0A%0AI%20would%20like%20to%20discuss%20a%20project%20with%20you.%0A%0ABest%20regards%2C%0AYour%20Name"
+                   class="btn secondary">Get in Touch
                 </a>
             </div>
         </div>
