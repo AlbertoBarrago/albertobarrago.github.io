c566e71a4aa0e0463cd253eda072c19b8024d3ed
Alberto Barrago <albertobarrago@gmail.com>
2025-03-22 17:35:15 +0100

    update cv name

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import pkg from '../../package.json'
 
 let name = "alBz";
 let role = "Fullstack DevLover";
-let cv_filename = "cv_2025_eng_albz.pdf"
+let cv_filename = "barrago_alberto_cv.pdf"
 let githubRepoString = "https://github.com/AlbertoBarrago?tab=repositories&catx=736&caty=799&catdx=1208&catdy=890&type=public"
 let tagline = "Building digital experiences that matter"
 let skills = [
